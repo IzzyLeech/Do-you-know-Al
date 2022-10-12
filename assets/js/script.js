32c6f62bcca1d208edded969eddf1eea806d3bd5
IzzyLeech <ralphhunter46@gmail.com>
2022-10-12 20:09:57 +0100

    add questions in script for quiz

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+let questions = [
+    {
+        question: `What is the name of the film that Al pacino has won his first and only Academy Award?`,
+        a: `Dog Day Afternoon`,
+        b: `The Godfather Part II`,
+        c: `Serpico`,
+        d: `Scent of a Woman`,
+        answer: `d`
+    },
+
+    {
+        question: `Al Pacino and Robert De Niro have starred in a total of four movies together, can you name the film that they first shared screen time in?`,
+        a: `Righteous Kill`,
+        b: `Heat`,
+        c: `The Godfather Part II`,
+        d: `The Irishman`,
+        answer: `b`
+    },
+
+    {
+        question: `Al Pacino has starred in numerous play's and their movie adaption, although not always playing the same character. 
+        Can you name the film/play where he play's the same character?`,
+        a: `Glengarry Glen Ross`,
+        b: `Salome`,
+        c: `The Merchant of Venice`,
+        d: `Julius Caesar`,
+        answer: `c`
+    },
+    
+    {
+        question: `Al Pacino starred in the cult classic Scarface in 1983 that was directed by Brain De Palma, 
+        but can you name the Academy Award winning Best Director that wrote the screenplay?`,
+        a: `Francis Ford Coppla`,
+        b: `Oliver Stone`,
+        c: `Mel Gibson`,
+        d: `Steven Spielberg`,
+        answer: `b`,
+    },
+
+    {
+        question: `Al Pacino has turend down multiple icon movie character throughout his career, can you pick the role he did play out of these roles?`,
+        a: `Han Sole (Star Wars)`,
+        b: `Ted Kramer (Kramer vs. Kramer)`,
+        c: `Carlito Brigante (Carlito's Way)`,
+        d: `Captain Willard (Apocalypse Now)`,
+        answer: `c`
+    }
+
+    
+
+
+
+]
