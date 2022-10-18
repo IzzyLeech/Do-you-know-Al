0b7e844003f2c7438644f3e9b5f37562e54d6d73
IzzyLeech <ralphhunter46@gmail.com>
2022-10-18 16:23:01 +0100

    add function to display difficulty question, plus css to the difficulty radio and question to medium and easy difficulty

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,79 +4,21 @@ let questionHolder = document.getElementById("question-holder");
 let scoreContainer = document.getElementById("score-container")
 let answers = document.querySelectorAll(".answer");
 
+let easyDifficulty = document.getElementById("easy-diff")
+let mediumDifficulty = document.getElementById("medium-diff")
+let hardDifficulty = document.getElementById("hard-diff")
+let difficultyLevel = "";
+
 let currentQuiz = 0;
 let score = 0;
 
-/**
-  * Button difficulty selected
-  */
-
-function chooseDifficulty() {
-    
-}
-
-startButton.addEventListener('click', startQuiz);
-
-function typeUsername (){
-    if(document.getElementById("username").value===""){
-        document.getElementById("start-btn").disabled = true;
-        
-    } else {
-        document.getElementById("start-btn").disabled = false;
-    }
-}
-
-function play() {
-    if(document.getElementById("username").value===""){
-        let give = document.getElementById("give-me")
-        give.play()
-    } else {
-        let waste = document.getElementById("waste-time");
-        waste.play()
-    }
-}
-
-/**
- * Function for transtion of the welcome menu to the questions
- */
-function startQuiz(){
-
-    welcome.classList.add("hide");
-    questionHolder.classList.remove('hide');
-
-    let name = document.getElementById("username").value;
-    document.getElementById("post").innerHTML = "Username:" + name ;
-
-
-    
-    let startTime = 1;
-    let time = startTime * 60;
-    let timer = document.getElementById("timer");
-
-    setInterval(countDown, 1000);
-
-    function countDown() {
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-
-    timer.innerHTML = `${minutes}: ${seconds}`;
-    time--;
-    }
-
-
-}
-
-/**
- * Question for the quiz
- */
-
 let questionsHard = [
     {
-        question: `What is the name of the film that Al pacino has won his first and only Academy Award?`,
-        a: `Dog Day Afternoon`,
-        b: `The Godfather Part II`,
-        c: `Serpico`,
-        d: `Scent of a Woman`,
+        question: `Al Pacino starred in the 1975 film Dog Day Afternoon that is the true story of a bank robbery gone bad. Where did the attempted robbery take place?`,
+        a: `Chicago`,
+        b: `Los Angeles`,
+        c: `Boston`,
+        d: `New York`,
         answer: `d`
     },
 
@@ -260,7 +202,146 @@ let questionsMedium = [
     }
 ]
 
-let questionEasy
+let questionsEasy = [
+    {
+        question:`Al Pacino starred as which character in the 1983 film Scarface?`,
+        a:`Indiana Jones`,
+        b:`James Bond`,
+        c:`Tony Montana`,
+        d:`Harry Potter`,
+        answer:`c`
+    },
+    {
+        question: `What is the name of the film that Al pacino has won his first and only Academy Award?`,
+        a: `Dog Day Afternoon`,
+        b: `The Godfather Part II`,
+        c: `Serpico`,
+        d: `Scent of a Woman`,
+        answer: `d`
+    },
+    {
+        question:`Al Pacino has not starrted in one of these movies, can you you pick the right movie?`,
+        a:`Heat`,
+        b:`Any Given Sunday`,
+        c:`Casino`,
+        d:`Carlito's way`,
+        answer:`c`
+    },
+    {
+        question:`Al Pacino utters the famous "You're Out of Order" line? in which movie?`,
+        a:`...And Justice for All`,
+        b:`The Devil's Advocate`,
+        c:`Scent of a Woman`,
+        d:`City Hall`,
+        answer:`a`
+    },
+    {
+        question:`Al Pacino starred in 2019 film The Irishman, can you pick the actor who doesn't co-star in the film`,
+        a:`Robert De Niro`,
+        b:`Christopher Walken`,
+        c:`Joe Pesci`,
+        d:`Harvey Keitel`,
+        answer:`b`
+    },
+    {
+        question:`Al Pacino portrays Tony D'Anato a head coach of the Miami Sharks in the 1999 movie Any Given Sunday, what sport do the Miami Sharks play?`,
+        a:`American Football`,
+        b:`Basketball`,
+        c:`Ice Hockey`,
+        d:`Baseball`,
+        answer:`a`
+    },
+    {
+        question:`Al Pacino starred alongside George Clooney, Brad Pitt and Matt Damon, in which 2007 heist movie?`,
+        a:`Ronin`,
+        b:`Ocean's Thirteen`,
+        c:`Inception`,
+        d:`The Town`,
+        answer:`b`
+    },
+    {
+        question:`Al Pacino portrays Michael Corleone in the Godfather Trilogy that was directed by which director?`,
+        a:`George Lucas`,
+        b:`Stanley Kubrick`,
+        c:`Francis Ford Coppola`,
+        d:`Roman Polanski`,
+        answer:`c`
+    },
+    {
+        question:`Al Pacino is known for many of his famous movie quotes, can you finish this quote "Say hello to my little ?"?`,
+        a:`Chicken`,
+        b:`Man`,
+        c:`Doggie`,
+        d:`Friend`,
+        answer:`d`
+    },
+    {
+        question:`Al Pacino starred alongisde which actor in the 1999 film The Insider, known for roles in movies such as Gladiator, American Gangster and A Beautiful Mind?`,
+        a:`Russel Crowe`,
+        b:`Tom Hanks`,
+        c:`Ed Harris`,
+        d:`Denzel Washington`,
+        answer:`a`
+    },
+    
+]
+
+/**
+  * Button difficulty selected
+  */
+startButton.addEventListener('click', startQuiz);
+
+function typeUsername (){
+    if(document.getElementById("username").value===""){
+        document.getElementById("start-btn").disabled = true;
+        
+    } else {
+        document.getElementById("start-btn").disabled = false;
+    }
+}
+
+function play() {
+    if(document.getElementById("username").value===""){
+        let give = document.getElementById("give-me")
+        give.play()
+    } else {
+        let waste = document.getElementById("waste-time");
+        waste.play()
+    }
+} 
+
+/**
+ * Function for transtion of the welcome menu to the questions
+ */
+function startQuiz(){
+
+    welcome.classList.add("hide");
+    questionHolder.classList.remove('hide');
+
+    let name = document.getElementById("username").value;
+    document.getElementById("post").innerHTML = "Username:" + name ;
+
+
+    
+    let startTime = 1;
+    let time = startTime * 60;
+    let timer = document.getElementById("timer");
+
+    setInterval(countDown, 1000);
+
+    function countDown() {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    timer.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+    }
+
+}
+
+/**
+ * Question for the quiz
+ */
 
 let questionE1 = document.getElementById("question")
 let a_answer = document.getElementById("a-answer");
@@ -271,16 +352,24 @@ let submitBtn = document.getElementById("submit");
 
 
 
-displayQuestion();
+easyDifficulty.addEventListener('click', displayEasyQuestion)
+mediumDifficulty.addEventListener('click', displayMediumQuestion)
+hardDifficulty.addEventListener('click', displayHardQuestion)
 
-/**
- * Function to display the question in the question holder.
- */
 
-function displayQuestion() {
-    deselectAnswer();
+let pickDifficulty = () => {
+    if (easyDifficulty.checked){
+        difficultyLevel = "easy";
+    } else if (mediumDifficulty.checked){
+        difficultyLevel = "medium";
+    }else if (hardDifficulty.checked){
+        difficultyLevel = "hard";
+    }
+    }
 
-    let currentQuestion = questionsHard[currentQuiz];
+function displayHardQuestion (){
+
+    let currentQuestion = questionsHard [currentQuiz];
 
     questionE1.innerText = currentQuestion.question
     a_answer.innerText = currentQuestion.a
@@ -288,6 +377,52 @@ function displayQuestion() {
     c_answer.innerText = currentQuestion.c
     d_answer.innerText = currentQuestion.d
 
+}
+
+function displayMediumQuestion(){
+
+    let currentQuestion = questionsMedium [currentQuiz];
+
+    questionE1.innerText = currentQuestion.question
+    a_answer.innerText = currentQuestion.a
+    b_answer.innerText = currentQuestion.b
+    c_answer.innerText = currentQuestion.c
+    d_answer.innerText = currentQuestion.d
+}
+
+function displayEasyQuestion(){
+
+    let currentQuestion = questionsEasy [currentQuiz];
+
+    questionE1.innerText = currentQuestion.question
+    a_answer.innerText = currentQuestion.a
+    b_answer.innerText = currentQuestion.b
+    c_answer.innerText = currentQuestion.c
+    d_answer.innerText = currentQuestion.d
+}
+
+displayQuestion();
+
+function displayQuestion () {
+    
+    let currentQuestion = questionsEasy || questionsMedium || questionsHard[currentQuiz];
+
+    if (pickDifficulty === "easy") {
+        return displayEasyQuestion;
+    } else if (pickDifficulty === "medium"){
+        return displayMediumQuestion;
+    } else if (pickDifficulty === "hard"){
+        return displayHardQuestion;
+    }
+
+    deselectAnswer();
+
+    questionE1.innerText = currentQuestion.question
+    a_answer.innerText = currentQuestion.a
+    b_answer.innerText = currentQuestion.b
+    c_answer.innerText = currentQuestion.c
+    d_answer.innerText = currentQuestion.d
+    
 }
 
 function selectedAnswer() {
@@ -319,10 +454,9 @@ submitBtn.addEventListener('click', () => {
 
 
     if(answer){
-        if (answer === questionsHard[currentQuiz].answer) {
+        if (answer === questionsHard[currentQuiz].answer)  {
             score++;
         }
-
         currentQuiz++;
         if (currentQuiz < questionsHard.length) {
             displayQuestion();
